87bb402dddc2150474e76ef20414a42b0b62e2b5
Ashmit <ashmitv704@gmail.com>
2024-11-20 19:34:10 +0530

    changes

--- Addition_of_Square_Matrix.c
+++ Addition_of_Square_Matrix.c
@@ -4,7 +4,6 @@ void main(){
 	int order;
 	printf("Enter the order of the matrix : ");
 	scanf("%d",&order);
-	
 	int m1[order][order],m2[order][order],r[order][order];
 	printf("For matrix 1 :::\n\n");
 	for (int i=0;i<order;i++){
@@ -20,18 +19,12 @@ void main(){
 			scanf("%d",&m2[i][j]);
 		}
 	}
-	
 	printf("Resultant matrix is :::\n ");
 	for (int i=0;i<order;i++){
 		for(int j=0;j<order;j++){
 			r[i][j]=((m1[i][j])+(m2[i][j]));
-		}	
-	}
-	for (int i=0;i<order;i++){
-		for(int j=0;j<order;j++){
 			printf("%d ",r[i][j]);
-
-		}
+		}	
 		printf("\n");
 	}
 }
